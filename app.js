da829160f65e237e177ad66c14bf8b0e7b26da3c
Ana Patiño <asofiapatino@unicesar.edu.co>
2022-03-27 16:02:20 -0500

    Update status and Delete activity

--- app.js
+++ app.js
@@ -23,15 +23,35 @@ const PaintDB = () => {
     activitiesArray = [];
   } else {
     activitiesArray.forEach((element) => {
-      actitiesListUI.innerHTML += `<div class="alert alert-danger" role="alert">
-        <i class="material-icons float-left mr-2"> accessibility </i>
-        <b>${element.activity}</b> - ${element.status}<span class="float-right"><i
-        class="material-icons"> done </i>
-        <i class="material-icons"> delete </i>
-        </span></div>`;
+     if (element.status) {
+      actitiesListUI.innerHTML += `<div class="alert alert-success" role="alert"><i class="material-icons float-left mr-2"> accessibility </i><b>${element.activity}</b> - ${element.status}<span class="float-right"><i class="material-icons"> done </i><i class="material-icons"> delete </i></span></div>`;
+     }
+     else {
+      actitiesListUI.innerHTML += `<div class="alert alert-danger" role="alert"><i class="material-icons float-left mr-2"> accessibility </i><b>${element.activity}</b> - ${element.status}<span class="float-right"><i class="material-icons"> done </i><i class="material-icons"> delete </i></span></div>`;
+     }
+      
     });
   }
 };
+
+const DeleteDB = (activity) => {
+  let indexArray;
+  activitiesArray.forEach((element, index) => {
+    if (element.activity === activity) {
+      indexArray = index;
+    }
+  });
+  activitiesArray.splice(indexArray, 1);
+  SaveDB();
+};
+
+const EditDB = (activity) => {
+ let indexArray = activitiesArray.findIndex((element) => {
+  return element.activity === activity
+ });
+ activitiesArray[indexArray].status = true;
+ SaveDB();
+}
 
 formUI.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -45,9 +65,10 @@ document.addEventListener("DOMContentLoaded", PaintDB);
 
 actitiesListUI.addEventListener("click", (e) => {
   e.preventDefault();
-
-  if (e.target.innerHTML === 'done' || e.target.innerHTML === 'delete') {
-     /* console.log(e.target.offSetParent.childNodes[3].innerHTMLL);*/
-     console.log(e.target.innerHTML);
+  const text = e.target.offSetParent.childNodes[1].innerHTMLL;
+  if (e.target.innerHTML === "done") {
+  }
+  if (e.target.innerHTML === "delete") {
+    DeleteDB(text);
   }
 });
