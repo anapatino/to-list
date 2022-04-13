1bffc62f0c270ef31d8bc94ce78ccc2a18ed52f0
Ana Patiño <asofiapatino@unicesar.edu.co>
2022-04-12 20:12:38 -0500

    runtine class readjustments

--- src/components/Rutine.js
+++ src/components/Rutine.js
@@ -8,8 +8,16 @@ export default class Rutine {
     document.addEventListener("DOMContentLoaded", this.paintDB);
   }
 
+  generateId = () => {
+    if (this.activitiesArray == null || this.activitiesArray.length == 0) {
+      return 0;
+    }
+    return this.activitiesArray[this.activitiesArray.length - 1].id + 1;
+  };
+
   createItem = (activity) => {
     let item = {
+      id: this.generateId(),
       activity: activity,
       status: "Pending",
     };
@@ -36,16 +44,16 @@ export default class Rutine {
 
   checkStatus = (element) => {
     if (element.status == "Completed") {
-      this.actitiesListUI.innerHTML += `<div class="alert alert-alert" role="alert"><i class="material-icons float-left mr-2">check_circle_outline</i><b>${element.activity}</b> - ${element.status}<span class="float-right"><i class="material-icons clickable">done</i><i class="material-icons clickable">delete</i></span></div>`;
+      this.actitiesListUI.innerHTML += `<div class="alert alert-alert" role="alert"><i class="material-icons float-left mr-2">check_circle_outline</i><b id="${element.id}">${element.activity}</b> - ${element.status}<span class="float-right"><i class="material-icons clickable">done</i><i class="material-icons clickable">delete</i></span></div>`;
     } else {
-      this.actitiesListUI.innerHTML += `<div class="alert alert-dark" role="alert"><i class="material-icons float-left mr-2">radio_button_unchecked</i><b>${element.activity}</b> - ${element.status}<span class="float-right"><i class="material-icons clickable">done</i><i class="material-icons clickable">delete</i></span></div>`;
+      this.actitiesListUI.innerHTML += `<div class="alert alert-dark" role="alert"><i class="material-icons float-left mr-2">radio_button_unchecked</i><b id="${element.id}">${element.activity}</b> - ${element.status}<span class="float-right"><i class="material-icons clickable">done</i><i class="material-icons clickable">delete</i></span></div>`;
     }
   };
 
-  deleteDB = (activity) => {
+  deleteDB = (id) => {
     let indexArray;
     this.activitiesArray.forEach((element, index) => {
-      if (element.activity == activity) {
+      if (element.id == id) {
         indexArray = index;
       }
     });
@@ -53,9 +61,9 @@ export default class Rutine {
     this.saveDB();
   };
 
-  editDB = (activity) => {
+  editDB = (id) => {
     let indexArray = this.activitiesArray.findIndex((element) => {
-      return element.activity == activity;
+      return element.id == id;
     });
     this.activitiesArray[indexArray].status = "Completed";
     this.saveDB();
@@ -74,27 +82,27 @@ export default class Rutine {
 
   getRutineRoute = (e) => {
     if (isFirefox()) {
-      return e.target.parentNode.parentNode.childNodes[1].innerHTML;
+      return e.target.parentNode.parentNode.childNodes[1].id;
     }
     // chrome
-    return e.path[2].childNodes[1].innerHTML;
+    return e.path[2].childNodes[1].id;
   };
 
   addModifyRutineEvent = () => {
     this.actitiesListUI.addEventListener("click", (e) => {
       e.preventDefault();
-      const text = this.getRutineRoute(e);
+      const id = this.getRutineRoute(e);
       const button = e.target.innerHTML;
-      this.chooseOption(text, button);
+      this.chooseOption(id, button);
     });
   };
 
-  chooseOption = (text, button) => {
+  chooseOption = (id, button) => {
     if (button == "done") {
-      this.editDB(text);
+      this.editDB(id);
     }
     if (button == "delete") {
-      this.deleteDB(text);
+      this.deleteDB(id);
     }
   };
 }
