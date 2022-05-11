9d6e71168e00e939956ed3a0882c55c37bba0017
Ana Patiño <asofiapatino@unicesar.edu.co>
2022-05-11 18:30:04 -0500

    implementing OOCSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,23 @@
 export const Footer = () => {
-  return /*html*/ `<footer class="footer" id="footer">
-<div class="fContainer">
+  return /*html*/ `<footer class="footer color" id="footer">
+<div class="fContainer gridRows">
   <section class="footerContainer">
-    <nav class="fNavegation">
-      <h2 class="footerTitle">ToList</h2>
-      <ul class="fNavegationList">
+    <nav class="fNavegation gridColumn">
+      <h2 class="footerTitle white">ToList</h2>
+      <ul class="fNavegationList columnItems">
         <li class="nItems">
-          <a href="#headboard" class="navLinks">Introduction</a>
+          <a href="#headboard" class="navLinks white">Introduction</a>
         </li>
         <li class="nItems">
-          <a href="#main" class="navLinks">Planning</a>
+          <a href="#main" class="navLinks white">Planning</a>
         </li>
         <li class="nItems">
-          <a href="#footer" class="navLinks">Contact</a>
+          <a href="#footer" class="navLinks white">Contact</a>
         </li>
       </ul>
     </nav>
   </section>
-  <section class="fContainerSocial">
+  <section class="fContainerSocial centerItems">
     <div class="fSocial">
       <a href="https://www.facebook.com/ana.sofia.patino.A/"  target="_blank" class="fIcons"><img src="../src/image/facebook.svg" class="fIcons"></a>
       <a href="https://github.com/anapatino"  target="_blank" class="fIcons"><img src="../src//image/github.svg" class="fIcons"></a>
@@ -25,7 +25,7 @@ export const Footer = () => {
       <a href="mailto:asofiapatinoaliendra@gmail.com"  target="_blank" class="fIcons"><img src="../src//image/email.svg" class="fIcons"></a>
     </div>
   </section>
- <h6 class="footerCopy">All rights reserved &copy; Ana Patiño & David Ravelo</h6>
+ <h6 class="footerCopy centerItems white">All rights reserved &copy; Ana Patiño & David Ravelo</h6>
 </div>
 </footer>`;
 };
